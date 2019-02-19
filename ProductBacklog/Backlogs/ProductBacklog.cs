4e137bf1e8fc1c44a2ce264384f4e0fde3cb2937
James Preston <jamescpreston93@gmail.com>
2019-02-19 18:58:47 +0000

    Add ProductBacklog class

---
+++ ProductBacklog/Backlogs/ProductBacklog.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ProductBacklog.BacklogItems;
+
+namespace ProductBacklog.Backlogs
+{
+    public class ProductBacklog
+    {
+        public string Name { get; set; }
+        public IList<BacklogItem> BacklogItems { get; set; }
+    }
+}
