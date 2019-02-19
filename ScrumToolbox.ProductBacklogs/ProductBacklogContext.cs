83fa5afe1e34c5ba86c6832d4e8d71972e53c27c
James Preston <jamescpreston93@gmail.com>
2019-02-19 21:13:50 +0000

    Add DbSets

--- ScrumToolbox.ProductBacklogs/ProductBacklogContext.cs
+++ ScrumToolbox.ProductBacklogs/ProductBacklogContext.cs
@@ -1,11 +1,14 @@
 ﻿using System.Data.Entity;
 using ScrumToolbox.ProductBacklogs.Tasks;
 using ScrumToolbox.ProductBacklogs.BacklogItems;
+using ScrumToolbox.ProductBacklogs.Backlogs;
 
 namespace ScrumToolbox.ProductBacklogs
 {
     public class ProductBacklogContext : DbContext
     {
-
+        public DbSet<ProductBacklog> ProductBacklogs { get; set; }
+        public DbSet<BacklogItem> BacklogItems { get; set; }
+        public DbSet<Task> Tasks { get; set; }
     }
 }
