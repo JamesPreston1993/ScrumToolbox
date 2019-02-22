72c7a20b68c7200ec608914684009958ac45bffe
James Preston <jamescpreston93@gmail.com>
2019-02-22 23:26:55 +0000

    Add get many product backlog items endpoint

---
+++ ScrumToolbox.WebApi/Controllers/ProductBacklogItemController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ScrumToolbox.ProductBacklogs;
+using ScrumToolbox.ProductBacklogs.BacklogItems;
+using ScrumToolbox.WebApi.Models;
+
+namespace ScrumToolbox.WebApi.Controllers
+{
+    [Route("api/backlogs/{productBacklogId}/pbis")]
+    public class ProductBacklogItemController : ControllerBase
+    {
+        private readonly ProductBacklogContext backlogContext;
+
+        public ProductBacklogItemController(ProductBacklogContext backlogContext)
+        {
+            this.backlogContext = backlogContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetMany(int productBacklogId)
+        {
+            var pbis = this.backlogContext
+                .BacklogItems
+                .Where(pbi => pbi.ProductBacklogId == productBacklogId)
+                .ToList();
+
+            return Ok(pbis);
+        }
+    }
+}
