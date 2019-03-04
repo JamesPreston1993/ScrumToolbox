9466fc39575911ee095c3516ef3a2c6d85d23778
James Preston <jamescpreston93@gmail.com>
2019-03-04 18:26:20 +0000

    Add Get method for PBI controller and tests

--- ScrumToolbox.WebApi.Tests/Controllers/ProductBacklogItemControllerTests.cs
+++ ScrumToolbox.WebApi.Tests/Controllers/ProductBacklogItemControllerTests.cs
@@ -49,5 +49,53 @@ namespace ScrumToolbox.WebApi.Tests.Controllers
                 Assert.True(pbis.All(pbi => pbi.ProductBacklogId == 1));
             }
         }
+
+        public class Get
+        {
+            private readonly ProductBacklogItemController controller;
+
+            public Get()
+            {
+                var context = new Mock<IProductBacklogContext>();
+                context.SetupGet(c => c.BacklogItems)
+                    .Returns(DbSetUtils.GetMockDbSet(
+                        new BacklogItem { Id = 1, ProductBacklogId = 1 },
+                        new BacklogItem { Id = 2, ProductBacklogId = 2 }
+                    ));
+                this.controller = new ProductBacklogItemController(context.Object);
+            }
+
+            [Fact]
+            public void ReturnsOk()
+            {
+                // act
+                var result = this.controller.Get(1, 1);
+
+                // assert
+                Assert.Equal(200, ((OkObjectResult)result).StatusCode);
+            }
+
+            [Fact]
+            public void ReturnsBadRequestWhenIdIsInvalid()
+            {
+                // act
+                var result = this.controller.Get(2, 1);
+
+                // assert
+                Assert.Equal(400, ((BadRequestObjectResult)result).StatusCode);
+                Assert.Equal("Could not find backlog item with specified id and/or backlog id.", ((BadRequestObjectResult)result).Value);
+            }
+
+            [Fact]
+            public void ReturnsBadRequestWhenBacklogIdIsInvalid()
+            {
+                // act
+                var result = this.controller.Get(1, 2);
+
+                // assert
+                Assert.Equal(400, ((BadRequestObjectResult)result).StatusCode);
+                Assert.Equal("Could not find backlog item with specified id and/or backlog id.", ((BadRequestObjectResult)result).Value);
+            }
+        }
     }
 }
--- ScrumToolbox.WebApi/Controllers/ProductBacklogItemController.cs
+++ ScrumToolbox.WebApi/Controllers/ProductBacklogItemController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using ScrumToolbox.ProductBacklogs;
-using ScrumToolbox.ProductBacklogs.BacklogItems;
-using ScrumToolbox.WebApi.Models;
 
 namespace ScrumToolbox.WebApi.Controllers
 {
@@ -15,6 +12,21 @@ namespace ScrumToolbox.WebApi.Controllers
         public ProductBacklogItemController(IProductBacklogContext backlogContext)
         {
             this.backlogContext = backlogContext;
+        }
+
+        [Route("{backlogItemId}")]
+        [HttpGet]
+        public IActionResult Get(int productBacklogId, int backlogItemId)
+        {
+            var productBacklogItem = this.backlogContext
+                .BacklogItems
+                .Where(pbi => pbi.ProductBacklogId == productBacklogId)
+                .SingleOrDefault(pbi => pbi.Id == backlogItemId);
+
+            if (productBacklogItem == null)
+                return BadRequest("Could not find backlog item with specified id and/or backlog id.");
+
+            return Ok(productBacklogItem);
         }
 
         [HttpGet]
