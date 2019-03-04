5e5a0adb85e4154192f3b405c4f7f1de418b79aa
James Preston <jamescpreston93@gmail.com>
2019-03-04 20:11:22 +0000

    Add create backlog item dto

---
+++ ScrumToolbox.WebApi/Models/BacklogItems/BacklogItemCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ScrumToolbox.WebApi.Models.BacklogItems
+{
+    public class BacklogItemCreateDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
