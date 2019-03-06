9dbe3a4ca8c71915ad1ab2650179af6df04ebe43
James Preston <jamescpreston93@gmail.com>
2019-03-06 17:25:18 +0000

    Cleanup web app project

--- ScrumToolbox.WebApp/ClientApp/src/components/NavMenu.js
+++ ScrumToolbox.WebApp/ClientApp/src/components/NavMenu.js
@@ -9,29 +9,20 @@ export class NavMenu extends Component {
 
   render() {
     return (
-      <Navbar inverse fixedTop fluid collapseOnSelect>
+      <Navbar inverse expanded fixedTop fluid collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
-            <Link to={'/'}>ScrumToolbox.WebApp</Link>
+            <Link to={'/'}>Scrum Toolbox</Link>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav>
             <LinkContainer to={'/'} exact>
-              <NavItem>
-                <Glyphicon glyph='home' /> Home
-              </NavItem>
+              <NavItem><Glyphicon glyph='home' /> Home</NavItem>
             </LinkContainer>
-            <LinkContainer to={'/counter'}>
-              <NavItem>
-                <Glyphicon glyph='education' /> Counter
-              </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/fetchdata'}>
-              <NavItem>
-                <Glyphicon glyph='th-list' /> Fetch data
-              </NavItem>
+            <LinkContainer to={'/backlogs'}>
+              <NavItem><Glyphicon glyph='th-list' /> Backlogs</NavItem>
             </LinkContainer>
           </Nav>
         </Navbar.Collapse>
